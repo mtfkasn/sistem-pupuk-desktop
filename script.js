95752cb3912b879f03ed7bdf6d3fe1c65a116770
mtfkasn <mtaufikahsan@gmail.com>
2026-01-06 07:12:17 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showToast() {
-    const toast = document.getElementById('toast');
-    toast.classList.remove('hidden');
-    
-    // Hilang otomatis setelah 3 detik
-    setTimeout(() => {
-        toast.classList.add('hidden');
-    }, 3000);
-}
