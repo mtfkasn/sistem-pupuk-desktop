dfa1f8ac6c0c98becf2243a538b65145ec18045b
mtfkasn <mtaufikahsan@gmail.com>
2026-01-06 08:02:57 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showToast() {
-    const toast = document.getElementById('toast');
-    toast.classList.remove('hidden');
-    
-    setTimeout(() => {
-        toast.classList.add('hidden');
-    }, 3000);
-}
